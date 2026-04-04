07cd5df1283bffb9be7fb274cc3a471e12a09fec
Tallya <ayllat2001@gmail.com>
2026-04-04 16:46:46 -0300

    add pg module

--- src/db/database.js
+++ src/db/database.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
+const pg = require('pg');
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -9,6 +10,7 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
+    dialectModule: pg,
     logging: false,
   }
 );
